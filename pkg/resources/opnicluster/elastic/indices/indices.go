894cc809c6b0430b00c9fcd2b18cabcb3c732eb7
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-10-19 11:38:03 -0700

    Revert changes to indices.go and opni_settings.go files

--- pkg/resources/opnicluster/elastic/indices/indices.go
+++ pkg/resources/opnicluster/elastic/indices/indices.go
@@ -225,7 +225,6 @@ func (r *Reconciler) Reconcile() (retResult *reconcile.Result, retErr error) {
 		}
 		policies = append(policies, opniDrainModelStatusPolicy)
 		policies = append(policies, opniMetricPolicy)
-		policies = append(policies, opniLogTemplatePolicy)
 	}
 	for _, policy := range policies {
 		err = r.osReconciler.ReconcileISM(policy)
@@ -239,7 +238,6 @@ func (r *Reconciler) Reconcile() (retResult *reconcile.Result, retErr error) {
 	templates := []esapiext.IndexTemplateSpec{
 		drainStatusTemplate,
 		opniMetricTemplate,
-		logTemplate,
 	}
 
 	if lo.FromPtrOr(r.spec.Opensearch.EnableLogIndexManagement, true) {
@@ -258,7 +256,6 @@ func (r *Reconciler) Reconcile() (retResult *reconcile.Result, retErr error) {
 	prefixes := map[string]string{
 		drainStatusIndexPrefix: drainStatusIndexAlias,
 		metricIndexPrefix:      metricIndexAlias,
-		logTemplateIndexPrefix: logTemplateIndexAlias,
 	}
 
 	if lo.FromPtrOr(r.spec.Opensearch.EnableLogIndexManagement, true) {
